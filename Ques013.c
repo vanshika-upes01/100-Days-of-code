f4770befa2a7f089c215e7671648362f5dd38387
Vanshika-upes01 <vanshika.agrl2502@gmail.com>
2025-12-03 03:52:48 +0000

    QUESTION 013

---
+++ Ques013.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int year;
+
+    // Input year from user
+    printf("Enter a year: ");
+    scanf("%d", &year);
+
+    // Check leap year conditions
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                printf("%d is a leap year.\n", year);
+            } else {
+                printf("%d is not a leap year.\n", year);
+            }
+        } else {
+            printf("%d is a leap year.\n", year);
+        }
+    } else {
+        printf("%d is not a leap year.\n", year);
+    }
+
+    return 0;
+}
