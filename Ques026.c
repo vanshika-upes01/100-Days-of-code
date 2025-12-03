3b51cc6229d9528c3ddbd4ee0e44a0d5b65d80a1
Vanshika-upes01 <vanshika.agrl2502@gmail.com>
2025-12-03 04:17:32 +0000

    QUESTION 026

---
+++ Ques026.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    // Input the value of n
+    printf("Enter a positive integer n: ");
+    scanf("%d", &n);
+
+    // Print numbers from 1 to n
+    for (i = 1; i <= n; i++) {
+        printf("%d ", i);
+    }
+    printf("\n");
+
+    return 0;
+}
