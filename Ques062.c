c553119203a94baea9b37896c57f055c093b8ce7
Vanshika-upes01 <vanshika.agrl2502@gmail.com>
2025-12-03 11:45:05 +0000

    QUESTION 062

---
+++ Ques062.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int n, temp;
+
+    // Input the size of the array
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Input array elements
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Reverse the array in place
+    for (int i = 0; i < n / 2; i++) {
+        temp = arr[i];
+        arr[i] = arr[n - 1 - i];
+        arr[n - 1 - i] = temp;
+    }
+
+    // Print the reversed array
+    printf("Reversed array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
