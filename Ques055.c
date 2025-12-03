0b42521fcf59cf80c2b7a717f22bada90e5517b6
Vanshika-upes01 <vanshika.agrl2502@gmail.com>
2025-12-03 11:37:46 +0000

    QUESTION 055

---
+++ Ques055.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, isPrime;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    printf("Prime numbers from 1 to %d are:\n", n);
+
+    for (i = 2; i <= n; i++) {   // Start from 2 since 1 is not prime
+        isPrime = 1;
+
+        for (j = 2; j <= i/2; j++) {
+            if (i % j == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+
+        if (isPrime == 1) {
+            printf("%d ", i);
+        }
+    }
+
+    return 0;
+}
