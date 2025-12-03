dfb4707635ed343033f6e1ff5c3e734985a7a42c
Vanshika-upes01 <vanshika.agrl2502@gmail.com>
2025-12-03 11:55:51 +0000

    QUESTION 071

---
+++ Ques071.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+
+    // Input number of rows and columns
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter the number of columns: ");
+    scanf("%d", &cols);
+
+    int matrix[rows][cols];
+
+    // Input matrix elements
+    printf("Enter the elements of the matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Print the matrix
+    printf("The matrix is:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
